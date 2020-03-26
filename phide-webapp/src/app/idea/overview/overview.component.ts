a594bfd1b4a5531b3fa944a4a45d5dbaefbe645d
Ammonix <joan.kuenzler@gmail.com>
2020-03-26 04:44:03 +0100

    responsive overview

--- phide-webapp/src/app/idea/overview/overview.component.ts
+++ phide-webapp/src/app/idea/overview/overview.component.ts
@@ -1,15 +1,8 @@
-import {
-  Component,
-  OnInit,
-  ChangeDetectionStrategy,
-  Input
-} from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { Idea } from "src/app/shared/types/idea";
 import { Select } from "@ngxs/store";
 import { IdeaState } from "../state/idea.state";
 import { Observable } from "rxjs";
-import { Dimension } from "src/app/shared/types/dimension";
-import { map } from "rxjs/operators";
 
 @Component({
   selector: "phide-overview",
@@ -18,24 +11,7 @@ import { map } from "rxjs/operators";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OverviewComponent implements OnInit {
-  @Select(IdeaState.ideas) _ideas$: Observable<Idea[]>;
-  ideas$ = this._ideas$.pipe(
-    map(i =>
-      i.map(j => ({
-        name: j.name,
-        dimensions: j.dimensions.reduce(
-          (p, c) => {
-            p.labels.push(c.dimension.name);
-            p.froms.push(c.dimension.from);
-            p.tos.push(c.dimension.to);
-            p.percentages.push(c.percentage);
-            return p;
-          },
-          { labels: [], froms: [], tos: [], percentages: [] }
-        )
-      }))
-    )
-  );
+  @Select(IdeaState.ideas) ideas$: Observable<Idea[]>;
   constructor() {}
   ngOnInit(): void {}
 }
